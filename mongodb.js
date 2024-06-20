cb4f71bd9f41f31a76163541bfdc63a8292f8633
Subasu <g.subash2805@gmail.com>
2024-06-20 19:42:47 +0530

    CRUD using MongoClient

--- mongodb.js
+++ mongodb.js
@@ -1,30 +1,117 @@
-// CRUD create read update delete
+const { MongoClient } = require('mongodb');
 
-const { MongoClient, ObjectID } = require('mongodb')
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+//InsertOne
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         const userDB=db.collection('users');
+//         userDB.insertOne(
+//             {
+//                 'name': 'subash',
+//                 'age': 21
+//             }
+//         )
+//     }).catch(err => console.log('Unable to connect to Server!', err));
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database!')
-    }
+//InsertMany
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         db.collection('users').insertMany([
+//             {
+//                 'name': 'ganesh',
+//                 'age': 25
+//             },{
+//                 'name': 'mani',
+//                 'age': 15
+//             }
+//         ]    
+//         )
+//     }).catch(err => console.log('Unable to connect to Server!', err));
 
-    const db = client.db(databaseName)
-    
-    // db.collection('users').deleteMany({
-    //     age: 27
-    // }).then((result) => {
-    //     console.log(result)
-    // }).catch((error) => {
-    //     console.log(error)
-    // })
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         const taskDB=db.collection('tasks');
+//         taskDB.insertMany([
+//             {
+//                 'description': 'Task-1',
+//                 'status':'completed'
+//             },{
+//                 'description': 'Task-2',
+//                 'status':'Not completed'
+//             },{
+//                 'description': 'Task-3',
+//                 'status':'completed'
+//             }
+//         ]    
+//         )
+//     }).catch(err => console.log('Unable to connect to Server!', err));
 
-    db.collection('tasks').deleteOne({
-        description: "Clean the house"
-    }).then((result) => {
-        console.log(result)
-    }).catch((error) => {
-        console.log(error)
-    })
-})
+// ReadOneDocument
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         const userDB = db.collection('users');
+//         const user = userDB.findOne({ 'name': 'subash' }).then(res => {
+//             console.log(res);
+//         }).catch(err => {
+//             console.log(err)
+//         })
+//     }).catch(err => console.log('Unable to connect to Server!', err));
+
+// UpdateOne Document
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         const userDB = db.collection('users');
+//         const user = userDB.updateOne({ 'name': 'subash' },{$set:{'name':'kamesh'}});
+//         user.then(res => {
+//             console.log(res);
+//         }).catch(err => {
+//             console.log(err)
+//         })
+//     }).catch(err => console.log('Unable to connect to Server!', err));
+
+
+// UpdateMany Document
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         const userDB = db.collection('users');
+//         const user = userDB.updateMany({ 'name': 'subash' },{$set:{'name':'kamesh'}});
+//         user.then(res => {
+//             console.log(res);
+//         }).catch(err => {
+//             console.log(err)
+//         })
+//     }).catch(err => console.log('Unable to connect to Server!', err));
+
+// DeleteOne
+// MongoClient.connect(connectionURL, {})
+//     .then((client) => {
+//         const db = client.db(databaseName);
+//         const userDB = db.collection('users');
+//         const user = userDB.deleteOne({ 'name': 'subash' });
+//         user.then(res => {
+//             console.log(res);
+//         }).catch(err => {
+//             console.log(err)
+//         })
+//     }).catch(err => console.log('Unable to connect to Server!', err));
+
+// DeleteMany
+MongoClient.connect(connectionURL, {})
+    .then((client) => {
+        const db = client.db(databaseName);
+        const userDB = db.collection('users');
+        const user = userDB.deleteMany({ 'name': 'kamesh' });
+        user.then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err)
+        })
+    }).catch(err => console.log('Unable to connect to Server!', err));
